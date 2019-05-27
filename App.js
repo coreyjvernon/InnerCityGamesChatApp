5e8fe163cedf8befb9e045a86a8ea12acd801f1f
Corey Vernon <coreyjvernon@gmail.com>
2019-05-27 12:56:43 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput 
+          placeholder="Phone number"
+          style={styles.input}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '90%',
+  },
+});
